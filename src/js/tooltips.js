9b4699283d4f916ad9f12224fccd999309a680d1
amiga909 <lfo@gmx.ch>
2021-01-17 09:11:07 +0100

    ..

--- src/js/tooltips.js
+++ src/js/tooltips.js
@@ -79,7 +79,7 @@ const CONFIG = {
     screen: "manual",
 
     text: "",
-    props: { offset: 8, maxWidth: "80vh" },
+    props: { offset: 8, maxWidth: "90vw" },
   },
   errorMsgTokens: {
     screen: "manual",
