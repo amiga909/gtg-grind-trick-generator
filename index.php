be413476075a2fc0ba387c5bd85e734b5db7472c
amiga909 <lfo@gmx.ch>
2021-01-19 20:26:52 +0100

    https

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+<?php 
+
+if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $location);
+    exit;
+}
+header( 'Location: /index.html' );  
+
+?>
