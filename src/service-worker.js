c8fdc4e9e1db808fcfc84717b581bf8ec845d56d
amiga909 <lfo@gmx.ch>
2021-02-07 16:26:25 +0100

    disable debug

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,10 +1,10 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
 importScripts('/node_modules/workbox-google-analytics/build/workbox-offline-ga.prod.js');
  
-
+/*
 workbox.setConfig({
   debug: true,
-});
+});*/
 
 import * as googleAnalytics from 'workbox-google-analytics';
 
