3ce0ffa6bc570ef8e306808062d0642296bb7cad
amiga909 <lfo@gmx.ch>
2021-02-08 15:25:25 +0100

    ..

--- src/js/app.js
+++ src/js/app.js
@@ -12,7 +12,7 @@ import { Scoreboard } from "./scoreboard";
 import { GameOverScreen } from "./gameover-screen";
 import { Audioplayer } from "./audioplayer";
 
-console.log("test 4")
+console.log("test 5")
 let CONFIG = "";
 
 const DISABLE_SOUND = true;
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
-importScripts('/node_modules/workbox-google-analytics/build/workbox-offline-ga.prod.js');
+
 
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
-googleAnalytics.initialize();
+
  
