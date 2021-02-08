7fdff692264f409d2a26aa9242690b8a9ae16b64
amiga909 <lfo@gmx.ch>
2021-02-08 15:16:42 +0100

    sw debugging

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,14 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
 importScripts('/node_modules/workbox-google-analytics/build/workbox-offline-ga.prod.js');
- 
-/*
-workbox.setConfig({
-  debug: true,
-});*/
 
-import * as googleAnalytics from 'workbox-google-analytics';
-
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 googleAnalytics.initialize();
  
- 
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
