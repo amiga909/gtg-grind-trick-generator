049434ebef1ec71c05074bf08d17a36b40f59025
amiga909 <lfo@gmx.ch>
2021-02-09 19:05:04 +0100

    ..

---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "globDirectory": "public/",
+  "globPatterns": [
+    "**/*.{js,png,ico,json,txt,mp3,eot,svg,ttf,woff,woff2,jpg,html,css}"
+  ],
+  "swDest": "public/sw.js",
+  "swSrc": "sw.js"
+};
