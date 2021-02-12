d56663befe10757830fb4df0491102209d67c23e
amiga909 <lfo@gmx.ch>
2021-02-12 12:53:08 +0100

    ..

---
+++ src/js/sw-handler.js
@@ -0,0 +1,50 @@
+import { Workbox, messageSkipWaiting, getSW } from "workbox-window";
+
+window.addEventListener("load", () => {
+  var isChrome =
+    /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+  if (isChrome && "serviceWorker" in navigator) {
+    handleSW();
+  }
+});
+
+function handleSW() {
+  const wb = new Workbox("/sw.js");
+  wb.getSW().then( (sw)=>{
+    console.log("getSW",sw)
+    document.getElementById("sw-info-text").innerHTML = sw.state;
+  })
+  wb.addEventListener("waiting", (event) => {
+    console.log("skip waiting");
+    wb.messageSkipWaiting();
+    window.location.reload();
+  });
+
+  wb.addEventListener("installed", (event) => {
+    console.log("installed", event);
+
+    if (!event.isUpdate) {
+      console.log("Service worker activated for the first time");
+    }
+    if (event.isUpdate) {
+      console.log("'Service worker update", event);
+    }
+  });
+
+  wb.addEventListener("message", (event) => {
+    console.log("message", event);
+  });
+  wb.addEventListener("controlling", (event) => {
+    console.log("controlling",event);
+  });
+  wb.addEventListener("activated", (event) => {
+    document.getElementById("sw-info-text").innerHTML = "activated";
+    console.log("activated ",event);
+  });
+
+  wb.register();
+ 
+   
+
+ 
+}
