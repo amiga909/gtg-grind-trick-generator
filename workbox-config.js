05bd167f524da8b296dd1945607908d7c22f98f5
amiga909 <lfo@gmx.ch>
2021-02-12 15:39:01 +0100

    ..

--- workbox-config.js
+++ workbox-config.js
@@ -1,10 +1,7 @@
 module.exports = {
-  globDirectory: "public/",
-  globPatterns: [
-    "**/*.{js,png,ico,json,txt,mp3,eot,svg,ttf,woff,woff2,jpg,html,css}",
+  "globDirectory": "public/",
+  "globPatterns": [
+    "**/*.{js,png,ico,json,txt,mp3,eot,svg,ttf,woff,woff2,jpg,html,css}"
   ],
-  swDest: "public/sw.js",
-  //directoryIndex: "public/index.html",
-  //cleanupOutdatedCaches: true,
- // offlineGoogleAnalytics: true,
+  "swDest": "public/sw.js"
 };
