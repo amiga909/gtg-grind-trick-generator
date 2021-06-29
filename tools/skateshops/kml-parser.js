bc08a0e8ad03a316d5c0ff10ba2cbbbb4de729f0
amiga909 <lfo@gmx.ch>
2021-06-29 20:22:51 +0200

    bladeshop map builder

---
+++ tools/skateshops/kml-parser.js
@@ -0,0 +1,178 @@
+const http = require("http");
+const fs = require('fs');
+const xml2js = require('xml2js');
+const axios = require('axios');
+
+
+const run = async function () {
+  const parser = new xml2js.Parser();
+  const data = fs.readFileSync(__dirname + '/result-backlinks.kml');
+  const parsed = await parser.parseStringPromise(data);
+  const processed = await processShops(parsed);
+  console.log("done")
+}
+
+const processShops = async function (result) {
+  const placemarks = result.kml.Document[0].Placemark
+  let i = 0;
+  for (let v of placemarks) {
+    i++;
+    const hasDescription = v.description && v.description[0] !== ""
+    v.description = hasDescription ? v.description : v.address;
+    // write each fetch result due to Google rate limitatin
+    if (!v.backlinkCount) {
+      v.backlinkCount = await getBackLinkCount(v);
+      writeFile(result)
+    }
+  }
+  return result;
+}
+// https://www.seobility.net/de/preise/ 
+// https://developers.google.com/custom-search/v1/reference/rest/v1/cse/list#request
+// key: google-api-javascript-client  AIzaSyDk4cDi3vOfAs6YAhCSVfbg4aVI_Ag2BBk
+// https://www.googleapis.com/customsearch/v1?key=INSERT_YOUR_API_KEY&cx=017576662512468239146:omuauf_lfve&q=lectures
+const getBackLinkCount = async function (shop) {
+  let shopUrl = shop.ExtendedData[0].Data[0].value[0];
+  shopUrl = shopUrl.replace(/https:\/\//g, "")
+  shopUrl = shopUrl.replace(/http:\/\//g, "")
+  shopUrl = shopUrl.replace(/www\./g, "")
+  shopUrl = shopUrl.replace(/\//g, "")
+  shopUrl = encodeURI(shopUrl)
+  const cx = "543afbfdba3002b23"
+  const key = "AIzaSyDk4cDi3vOfAs6YAhCSVfbg4aVI_Ag2BBk"
+  let url = `https://www.googleapis.com/customsearch/v1?key=${key}&cx=${cx}&q=%22_SHOP_%22+-site%3A_SHOP_`
+  //url = "https://www.google.com/search?q=%22_SHOP_%22+-site%3A_SHOP_"
+  url = url.replace(/_SHOP_/g, shopUrl)
+  console.log("wait 2s")
+  await timeout(2000);
+  let response = await axios.get(url)
+  const count = response.data.searchInformation.totalResults
+
+  console.log("fetched", shop.ExtendedData[0].Data[0].value[0], ": ", count)
+  return count;
+}
+
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function writeFile(result) {
+  const builder = new xml2js.Builder();
+  const xml = builder.buildObject(result);
+  fs.writeFileSync(__dirname + '/result.kml', xml);
+  console.log("write file done")
+}
+
+
+run()
+
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(" ");
+  })
+  .listen(1338, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:1338/');
+
+
+/*
+
+"nickelanddimela.com" -site:nickelanddimela.com
+
+
+"rollingrock.ch" -site:rollingrock.ch 996
+"ovskate.com" -site:ovskate.com 975
+"aightgame.com" -site:aightgame.com  9
+
+"locoskates.com" -site:locoskates.com   7’480
+"grindhouse.eu" -site:grindhouse.eu 9000
+roex.es 9500
+
+
+
+"slickwillies.co.uk" -site:slickwillies.co.uk  6’900 / 75?
+"wl33.com" -site:wl33.com  9
+"aightgame.com" -site:aightgame.com  9
+
+
+wl33.com .. too much!
+sola: 500
+
+
+
+"goroller.com.br" -site:goroller.com.br 657
+"themgoods.com" -site:themgoods.com 6460
+"thuroshop.com" -site:thuroshop.com 2000
+"rollergrind360.com" -site:rollergrind360.com 2600
+
+
+"asphaltbeach.com" -site:asphaltbeach.com 439
+"skateworld.ro" -site:skateworld.ro  5’260
+"shop-laced.com" -site:shop-laced.com 1000
+"roces.com" -site:roces.com 10000
+"straightjacket.bigcartel.com" -site:straightjacket.bigcartel.com 400
+"skatepro.com" -site:skatepro.com 100'000
+
+
+"rollerwarehouse.com" -site:rollerwarehouse.com  9’430
+"xxxx" -site:xxxx
+"xxxx" -site:xxxx
+"xxxx" -site:xxxx
+
+
+
+/doblevdoble.com/es/ :
+
+
+Little blade shop project
+
+Can tell:
+- Find blade shops near to you
+- Which countries have a lot of blade shops
+Was surprising for me UK looks very good here.
+One explanation for this is skate shops in UK were always diversified and could survive this way.
+
+countries with a high blade shop density.
+Was surprising for me UK has the best blade shop per capita metric.
+
+
+- Helps to identify shops that probably have a bigger sortiment.
+- Sites with a very low backlink count might be less legit than sites with a high backlink count.
+
+
+- Tech details for the number
+Google Backlink metric:
+Tells how many other sites link to that site
+Tells how important that site is for Google
+
+
+SEO analysis techniques
+Domain Rating (DR) techniques
+Shops like proskate.com offer a lot of goods, not only blade customers
+
+
+If you want more details, feel free to ask.
+It is just an experimental SEO metric.
+Don't take it too serioulsy I wanted to extend my geographic blade shop analysis with a VERY rough qualitative metric.
+If you are a shop owner and you feel I trashed your shop, I am happy to share my knowledge.
+It does not say a shop is "bad", it just says how important a site might be in the context of Google searches.
+Some shops don't bother about being visible on Google, it can be an indicator for a professional site Google ranking is important.
+Well possible the "best" shop does not bother to be found on Google.
+Does not mean a shhop with a low "x" is not legit.
+It might point out a shop could invest more into SEO if the shop cares to perform well in the context of Google searches.
+Personally, I think the most interesting stuff happens in the underground.
+You can think of it as a metric how "mainstream" a shop is.
+
+
+I am aware this is too techy but I don't care about people that don't know afor a FB group that is mainly about watching blade vids.
+
+A few people appreciated my effort to share this, that's enough for me.
+Happy to take constructive critic, happy to take reasons why this is bad.
+Everybody can express their opinion, I like to focus on what looks like an informed opinion to me.
+
+
+why it is bad.
+
+
+*/
