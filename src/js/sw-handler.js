de2c2f98c36e35ff04ac0e6c9b0dfef2a39b4e67
amiga909 <lfo@gmx.ch>
2021-11-01 17:07:09 +0100

    unreg

--- src/js/sw-handler.js
+++ src/js/sw-handler.js
@@ -1,17 +1,22 @@
 window.addEventListener("load", () => {
+  // remove SWs for now
   unregister();
-  
+
+});
+
+function register() {
   let isChrome =
     /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
   if (isChrome && "serviceWorker" in navigator) {
     navigator.serviceWorker.register("/sw.js");
   }
-});
+}
 
 
 function unregister() {
-  navigator.serviceWorker.getRegistrations().then(function(registrations) {
-    for(let registration of registrations) {
-     registration.unregister()
-   } })
+  navigator.serviceWorker.getRegistrations().then(function (registrations) {
+    for (let registration of registrations) {
+      registration.unregister()
+    }
+  })
 }
