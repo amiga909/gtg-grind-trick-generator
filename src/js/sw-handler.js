da34ba7fd80f6e0a37e606c9119535f953527580
amiga909 <lfo@gmx.ch>
2021-11-01 17:01:16 +0100

    unreg

--- src/js/sw-handler.js
+++ src/js/sw-handler.js
@@ -1,8 +1,17 @@
 window.addEventListener("load", () => {
-  navigator.serviceWorker.unregister("/sw.js");
+  unregister();
+  
   let isChrome =
     /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
   if (isChrome && "serviceWorker" in navigator) {
     navigator.serviceWorker.register("/sw.js");
   }
 });
+
+
+function unregister() {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for(let registration of registrations) {
+     registration.unregister()
+   } })
+}
