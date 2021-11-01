798742be9e1df9e389eb27d98eede7a52470feb0
amiga909 <lfo@gmx.ch>
2021-11-01 16:55:24 +0100

    unregister sw

--- src/js/sw-handler.js
+++ src/js/sw-handler.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", () => {
+  navigator.serviceWorker.unregister("/sw.js");
   let isChrome =
     /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
   if (isChrome && "serviceWorker" in navigator) {
